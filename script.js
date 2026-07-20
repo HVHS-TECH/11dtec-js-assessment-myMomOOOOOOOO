df4f1600b1342e0f98426b5fa99a9aa35f682a47
LewisBassett888 <24114lb@hvhs.school.nz>
2026-07-20 21:24:29 +0000

    Jul 20, 2026, 9:24 PM

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function login() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
-    if (username == "admin" && password == "pizza") {
+    if (username == "username" && password == "password") {
         alert("Welcome!");
 
         localStorage.setItem("loggedIn", "true");
